579dd10a5636e56e290d2ff0e3eee96408c6b047
Brian Bickerton <brian.bickerton@gmail.com>
2016-01-09 06:45:36 +1100

    Requirements in response creation for result and error combinations

--- response.go
+++ response.go
@@ -30,6 +30,22 @@ func NewResponse(req *Request, result interface{}, err error) (*Response, error)
 		return nil, err
 	}
 
+	if result != nil && err != nil {
+		err := errors.New("cannot set both result and err")
+		log.WithFields(log.Fields{
+			"errors": err,
+		}).Error(err)
+		return nil, err
+	}
+
+	if result == nil && err == nil {
+		err := errors.New("must set one of either result or err")
+		log.WithFields(log.Fields{
+			"errors": err,
+		}).Error(err)
+		return nil, err
+	}
+
 	return &Response{
 		ID:     req.ID,
 		Result: result,
