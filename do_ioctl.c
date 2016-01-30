4d1e9b4d569d2293fdb07023138297d64b4733c1
Manuel Mendez <mmendez534@gmail.com>
2016-01-30 14:26:56 -0500

    handle zc_real_err from latest patchset versions

--- do_ioctl.c
+++ do_ioctl.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <limits.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -7,18 +8,19 @@
 #include <sys/types.h>
 
 #define ZFS_IOC_LIBZFS_CORE (('Z' << 8) + 0x100 + 1)
-struct zfs_cmd_new {
+struct zfs_cmd_stable {
 	char		zc_name[PATH_MAX]; 	/* name of pool or dataset */
 	uint64_t	zc_nvlist_src;		/* really (char *) */
 	uint64_t	zc_nvlist_src_size;
 	uint64_t	zc_nvlist_dst;		/* really (char *) */
 	uint64_t	zc_nvlist_dst_size;
-	boolean_t	zc_nvlist_dst_filled;	/* put an nvlist in dst? */
+	bool		zc_nvlist_dst_filled;	/* put an nvlist in dst? */
+	int32_t		zc_real_err;		/* true error is passed here */
 };
 
 typedef struct zfs_cmd {
-	struct zfs_cmd_new;
-	char _legacy_zfs_cmd_fields[(14 * 1024) - sizeof(struct zfs_cmd_new)];
+	struct zfs_cmd_stable;
+	char _legacy_zfs_cmd_fields[(14 * 1024) - sizeof(struct zfs_cmd_stable)];
 } zfs_cmd_t;
 
 int do_ioctl(int fd, char *name, int len, void *innvl, int insize, void *outnvl, int outsize) {
@@ -30,5 +32,10 @@ int do_ioctl(int fd, char *name, int len, void *innvl, int insize, void *outnvl,
 	cmd.zc_nvlist_dst = (uint64_t)outnvl;
 	cmd.zc_nvlist_dst_size = outsize;
 	cmd.zc_nvlist_dst_filled = outnvl != NULL;
-	return ioctl(fd, ZFS_IOC_LIBZFS_CORE, (unsigned long)&cmd);
+
+	int err = ioctl(fd, ZFS_IOC_LIBZFS_CORE, (unsigned long)&cmd);
+	if (err = 0) {
+		errno = cmd.zc_real_err;
+	}
+	return err;
 }
