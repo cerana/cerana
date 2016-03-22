8a5f31aada5a42e1bd36c5aeacb2f73f83d14054
Brian Bickerton <brian.bickerton@gmail.com>
2016-03-22 22:09:35 +0000

    Add Systemd Provider Remove and test

---
+++ providers/systemd/remove.go
@@ -0,0 +1,33 @@
+package systemd
+
+import (
+	"errors"
+	"net/url"
+	"os"
+
+	"github.com/mistifyio/mistify/acomm"
+)
+
+// RemoveArgs are arguments for the Remove handler.
+type RemoveArgs struct {
+	Name string `json:"name"`
+}
+
+// Remove deletes a unit file.
+func (s *Systemd) Remove(req *acomm.Request) (interface{}, *url.URL, error) {
+	var args RemoveArgs
+	if err := req.UnmarshalArgs(&args); err != nil {
+		return nil, nil, err
+	}
+
+	if args.Name == "" {
+		return nil, nil, errors.New("missing arg: name")
+	}
+
+	unitFilePath, err := s.config.UnitFilePath(args.Name)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return nil, nil, os.Remove(unitFilePath)
+}
--- providers/systemd/systemd.go
+++ providers/systemd/systemd.go
@@ -29,11 +29,12 @@ func New(config *Config) *Systemd {
 
 // RegisterTasks registers all of Systemd's task handlers with the server.
 func (s *Systemd) RegisterTasks(server *provider.Server) {
-	server.RegisterTask("systemd-create", s.Disable)
+	server.RegisterTask("systemd-create", s.Create)
 	server.RegisterTask("systemd-disable", s.Disable)
 	server.RegisterTask("systemd-enable", s.Enable)
 	server.RegisterTask("systemd-get", s.Get)
 	server.RegisterTask("systemd-list", s.List)
+	server.RegisterTask("systemd-remove", s.Remove)
 	server.RegisterTask("systemd-restart", s.Restart)
 	server.RegisterTask("systemd-start", s.Start)
 	server.RegisterTask("systemd-stop", s.Stop)
