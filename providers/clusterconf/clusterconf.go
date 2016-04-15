d899d0010443086542ee48f12f2e920358c37865
Brian Bickerton <brian.bickerton@gmail.com>
2016-04-15 21:58:42 +0000

    Cluster Config Dataset

---
+++ providers/clusterconf/clusterconf.go
@@ -0,0 +1,115 @@
+package clusterconf
+
+import (
+	"time"
+
+	"github.com/cerana/cerana/acomm"
+	"github.com/cerana/cerana/provider"
+	"github.com/mistifyio/lochness/pkg/kv"
+)
+
+// ClusterConf is a provider of cluster configuration functionality.
+type ClusterConf struct {
+	config  *Config
+	tracker *acomm.Tracker
+}
+
+// IDArgs are arguments for operations requiring only an ID.
+type IDArgs struct {
+	ID string `json:"id"`
+}
+
+// New creates a new instance of ClusterConf
+func New(config *Config, tracker *acomm.Tracker) *ClusterConf {
+	return &ClusterConf{
+		config:  config,
+		tracker: tracker,
+	}
+}
+
+// RegisterTasks registers all of Systemd's task handlers with the server.
+func (c *ClusterConf) RegisterTasks(server *provider.Server) {
+}
+
+func (c *ClusterConf) kvReq(task string, args map[string]interface{}) (*acomm.Response, error) {
+	respChan := make(chan *acomm.Response, 1)
+	defer close(respChan)
+	rh := func(_ *acomm.Request, resp *acomm.Response) {
+		respChan <- resp
+	}
+
+	req, err := acomm.NewRequest(acomm.RequestOptions{
+		Task:           task,
+		ResponseHook:   c.tracker.URL(),
+		Args:           args,
+		SuccessHandler: rh,
+		ErrorHandler:   rh,
+	})
+	if err := c.tracker.TrackRequest(req, 0); err != nil {
+		return nil, err
+	}
+	if err := acomm.Send(c.config.CoordinatorURL(), req); err != nil {
+		c.tracker.RemoveRequest(req)
+		return nil, err
+	}
+
+	return <-respChan, err
+}
+
+func (c *ClusterConf) kvGetAll(key string) (map[string]kv.Value, error) {
+	args := map[string]interface{}{
+		"key": key,
+	}
+
+	resp, err := c.kvReq("kv-getAll", args)
+	if err != nil {
+		return nil, err
+	}
+	values := make(map[string]kv.Value)
+	if err := resp.UnmarshalResult(values); err != nil {
+		return nil, err
+	}
+	return values, nil
+}
+
+func (c *ClusterConf) kvDelete(key string, modIndex uint64) error {
+	args := map[string]interface{}{
+		"key":     key,
+		"recurse": true,
+	}
+	resp, err := c.kvReq("kv-delete", args)
+	if err != nil {
+		return err
+	}
+	return resp.Error
+}
+
+func (c *ClusterConf) kvUpdate(key string, value interface{}, modIndex uint64) (uint64, error) {
+	args := map[string]interface{}{
+		"key":   key,
+		"value": value,
+		"index": modIndex,
+	}
+	resp, err := c.kvReq("kv-update", args)
+	if err != nil {
+		return 0, err
+	}
+	result := make(map[string]uint64)
+	if err := resp.UnmarshalResult(result); err != nil {
+		return 0, err
+	}
+	return result["index"], nil
+}
+
+func (c *ClusterConf) kvEphemeral(key string, value interface{}, ttl time.Duration) error {
+	args := map[string]interface{}{
+		"key":   key,
+		"value": value,
+		"ttl":   ttl,
+	}
+	resp, err := c.kvReq("kv-ephemeral", args)
+	if err != nil {
+		return err
+	}
+	return resp.Error
+}
--- providers/clusterconf/dataset.go
+++ providers/clusterconf/dataset.go
@@ -1,15 +1,193 @@
 package clusterconf
 
-import "net"
+import (
+	"encoding/json"
+	"errors"
+	"net"
+	"net/url"
+	"path"
+	"path/filepath"
 
-// Dataset is configuration for a dataset.
+	"github.com/cerana/cerana/acomm"
+)
+
+const datasetsPrefix string = "datasets"
+
+// Dataset is information about a dataset.
 type Dataset struct {
-	ID                string          `json:"id"`
-	Parent            string          `json:"parent"`
-	ParentSameMachine bool            `json:"parentSameMachine"`
-	ReadOnly          bool            `json:"readOnly"`
-	NFS               bool            `json:"nfs"`
-	Redundancy        int             `json:"redundancy"`
-	Quota             int             `json:"quota"`
-	Nodes             map[net.IP]bool `json:"nodes"`
+	*DatasetConf
+	c *ClusterConf
+	// Nodes contains the set of nodes on which the dataset is currently in use.
+	// The map keys are IP address strings.
+	Nodes map[string]bool `json:"nodes,omitempty"`
+	// ModIndex should be treated as opaque, but passed back on updates.
+	ModIndex uint64 `json:"modIndex,omitempty"`
+}
+
+// DatasetConf is the configuration of a dataset.
+type DatasetConf struct {
+	ID                string `json:"id"`
+	Parent            string `json:"parent"`
+	ParentSameMachine bool   `json:"parentSameMachine"`
+	ReadOnly          bool   `json:"readOnly"`
+	NFS               bool   `json:"nfs"`
+	Redundancy        int    `json:"redundancy"`
+	Quota             int    `json:"quota"`
+}
+
+// DatasetPayload can be used for task args or result when a dataset object
+// needs to be sent.
+type DatasetPayload struct {
+	Dataset *Dataset `json:"dataset"`
+}
+
+// DatasetHeartbeatArgs are arguments for updating a dataset node heartbeat.
+type DatasetHeartbeatArgs struct {
+	ID string `json:"id"`
+	IP net.IP `json:"ip"`
+}
+
+// GetDataset retrieves a dataset.
+func (c *ClusterConf) GetDataset(req *acomm.Request) (interface{}, *url.URL, error) {
+	var args IDArgs
+	if err := req.UnmarshalArgs(&args); err != nil {
+		return nil, nil, err
+	}
+	if args.ID == "" {
+		return nil, nil, errors.New("missing arg: id")
+	}
+
+	dataset, err := c.getDataset(args.ID)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &DatasetPayload{dataset}, nil, nil
+}
+
+// UpdateDataset creates or updates a dataset config. When updating, a Get should first be performed and the modified Dataset passed back.
+func (c *ClusterConf) UpdateDataset(req *acomm.Request) (interface{}, *url.URL, error) {
+	var args DatasetPayload
+	if err := req.UnmarshalArgs(&args); err != nil {
+		return nil, nil, err
+	}
+	if args.Dataset != nil {
+		return nil, nil, errors.New("missing arg: dataset")
+	}
+	args.Dataset.c = c
+
+	if err := args.Dataset.update(); err != nil {
+		return nil, nil, err
+	}
+	return &DatasetPayload{args.Dataset}, nil, nil
+}
+
+// DeleteDataset deletes a dataset config.
+func (c *ClusterConf) DeleteDataset(req *acomm.Request) (interface{}, *url.URL, error) {
+	var args IDArgs
+	if err := req.UnmarshalArgs(&args); err != nil {
+		return nil, nil, err
+	}
+	if args.ID == "" {
+		return nil, nil, errors.New("missing arg: id")
+	}
+
+	dataset, err := c.getDataset(args.ID)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return nil, nil, dataset.delete()
+}
+
+// DatasetHeartbeat registers a new node heartbeat that is using the dataset.
+func (c *ClusterConf) DatasetHeartbeat(req *acomm.Request) (interface{}, *url.URL, error) {
+	var args DatasetHeartbeatArgs
+	if err := req.UnmarshalArgs(&args); err != nil {
+		return nil, nil, err
+	}
+	if args.ID == "" {
+		return nil, nil, errors.New("missing arg: ID")
+	}
+	if args.IP == nil {
+		return nil, nil, errors.New("missing arg: IP")
+	}
+
+	dataset, err := c.getDataset(args.ID)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := dataset.nodeHeartbeat(args.IP); err != nil {
+		return nil, nil, err
+	}
+
+	return &DatasetPayload{dataset}, nil, nil
+}
+
+func (c *ClusterConf) getDataset(id string) (*Dataset, error) {
+	dataset := &Dataset{
+		c:           c,
+		DatasetConf: &DatasetConf{ID: id},
+	}
+	if err := dataset.reload(); err != nil {
+		return nil, err
+	}
+	return dataset, nil
+}
+
+func (d *Dataset) reload() error {
+	var err error
+	key := path.Join(datasetsPrefix, d.ID)
+	values, err := d.c.kvGetAll(key) // Blocking
+	if err != nil {
+		return err
+	}
+
+	// Config
+	config, ok := values[path.Join(key, "config")]
+	if !ok {
+		return errors.New("dataset config not found")
+	}
+	if err = json.Unmarshal(config.Data, d.DatasetConf); err != nil {
+		return err
+	}
+	d.ModIndex = config.Index
+
+	// Nodes
+	d.Nodes = make(map[string]bool)
+	for key := range values {
+		base := filepath.Base(key)
+		dir := filepath.Base(filepath.Dir(key))
+		if dir == "nodes" {
+			d.Nodes[base] = true
+		}
+	}
+
+	return nil
+}
+
+func (d *Dataset) delete() error {
+	key := path.Join(datasetsPrefix, d.ID)
+	return d.c.kvDelete(key, d.ModIndex)
+}
+
+// update saves the core dataset config. It will not modify nodes.
+func (d *Dataset) update() error {
+	key := path.Join(datasetsPrefix, d.ID, "config")
+
+	_, err := d.c.kvUpdate(key, d.DatasetConf, d.ModIndex)
+	if err != nil {
+		return err
+	}
+
+	// reload instead of just setting the new modIndex in case any nodes have also changed.
+	return d.reload()
+}
+
+func (d *Dataset) nodeHeartbeat(ip net.IP) error {
+	key := path.Join(datasetsPrefix, d.ID, "nodes", ip.String())
+	if err := d.c.kvEphemeral(key, true, d.c.config.DatasetTTL()); err != nil {
+		return err
+	}
+	return d.reload()
 }
