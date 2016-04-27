dd75027fdbf97f47ee30067011983307dc440df1
Brian Bickerton <brian.bickerton@gmail.com>
2016-04-27 20:17:25 +0000

    Linting fixes for clusterconf and test coordinator

--- pkg/test/coordinator.go
+++ pkg/test/coordinator.go
@@ -14,6 +14,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Coordinator holds a coordinator server and a provider server with one or
+// more registered mock Providers to be used for testing.
 type Coordinator struct {
 	SocketDir      string
 	coordinatorURL string
@@ -22,6 +24,8 @@ type Coordinator struct {
 	providerServer *provider.Server
 }
 
+// NewCoordinator creates a new Coordinator. The coordinator sever will be
+// given a temporary socket directory and external port.
 func NewCoordinator(baseDir string) (*Coordinator, error) {
 	coordinatorName := uuid.New()
 	socketDir, err := ioutil.TempDir(baseDir, coordinatorName)
@@ -32,13 +36,13 @@ func NewCoordinator(baseDir string) (*Coordinator, error) {
 	coordinatorViper := viper.New()
 	coordinatorViper.Set("service_name", coordinatorName)
 	coordinatorViper.Set("socket_dir", socketDir)
-	coordinatorViper.Set("external_port", 10000+rand.Intn(10000))
+	coordinatorViper.Set("external_port", 1024+rand.Intn(65535-1024))
 	coordinatorViper.Set("request_timeout", 20)
 	coordinatorViper.Set("log_level", "fatal")
 
 	flags := pflag.NewFlagSet(coordinatorName, pflag.ContinueOnError)
 	coordinatorConfig := coordinator.NewConfig(flags, coordinatorViper)
-	if err := flags.Parse([]string{}); err != nil {
+	if err = flags.Parse([]string{}); err != nil {
 		return nil, err
 	}
 	_ = coordinatorConfig.SetupLogging()
@@ -62,7 +66,7 @@ func NewCoordinator(baseDir string) (*Coordinator, error) {
 	c.providerName = uuid.New()
 	providerFlags := pflag.NewFlagSet(c.providerName, pflag.ContinueOnError)
 	providerConfig := provider.NewConfig(providerFlags, c.NewProviderViper())
-	if err := providerFlags.Parse([]string{}); err != nil {
+	if err = providerFlags.Parse([]string{}); err != nil {
 		return nil, err
 	}
 
@@ -75,6 +79,8 @@ func NewCoordinator(baseDir string) (*Coordinator, error) {
 	return c, nil
 }
 
+// NewProviderViper prepares a basic viper instance for a Provider, setting
+// appropriate values corresponding to the coordinator and provider server.
 func (c *Coordinator) NewProviderViper() *viper.Viper {
 	v := viper.New()
 	v.Set("service_name", c.providerName)
@@ -85,10 +91,13 @@ func (c *Coordinator) NewProviderViper() *viper.Viper {
 	return v
 }
 
+// RegisterProvider registers a Provider's tasks with the internal Provider
+// server.
 func (c *Coordinator) RegisterProvider(p provider.Provider) {
 	p.RegisterTasks(*c.providerServer)
 }
 
+// Start starts the Coordinator and Provider servers.
 func (c *Coordinator) Start() error {
 	if err := c.coordinator.Start(); err != nil {
 		return err
@@ -99,11 +108,13 @@ func (c *Coordinator) Start() error {
 	return nil
 }
 
+// Stop stops the Coordinator and Provider servers.
 func (c *Coordinator) Stop() {
 	c.providerServer.Stop()
 	c.coordinator.Stop()
 }
 
+// Cleanup removes the temporary socket directory.
 func (c *Coordinator) Cleanup() error {
 	return os.RemoveAll(c.SocketDir)
 }
