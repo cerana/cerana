41a381759df7c92d54ee3b51ed06b170b5870885
Brian Bickerton <brian.bickerton@gmail.com>
2016-05-02 19:14:32 +0000

    Expand stats pusher config and tests

--- cmd/statspusher/config_test.go
+++ cmd/statspusher/config_test.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"strconv"
 	"testing"
+	"time"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/pborman/uuid"
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/suite"
 )
 
 type StatsPusher struct {
 	suite.Suite
+	config     *config
+	configData *ConfigData
+	configFile *os.File
 }
 
 func TestStatsPusher(t *testing.T) {
 	suite.Run(t, new(StatsPusher))
+}
+
+func (s *StatsPusher) SetupTest() {
+	logrus.SetLevel(logrus.FatalLevel)
+
+	s.configData = &ConfigData{
+		CoordinatorURL: "unix:///tmp/foobar",
+		LogLevel:       "fatal",
+		RequestTimeout: 5,
+		DatasetTTL:     4,
+		BundleTTL:      3,
+		NodeTTL:        2,
+	}
+
+	var err error
+	s.config, _, _, s.configFile, err = newTestConfig(false, true, s.configData)
+	s.Require().NoError(err, "failed to create config")
+	s.Require().NoError(s.config.loadConfig(), "failed to load config")
+}
+
+func (s *StatsPusher) TearDownTest() {
+	_ = os.Remove(s.configFile.Name())
 }
 
 func (s *StatsPusher) TestCanonicalFlagName() {
@@ -45,4 +80,177 @@ func (s *StatsPusher) TestCanonicalFlagName() {
 	for _, test := range tests {
 		s.Equal(test.output, string(canonicalFlagName(pflag.CommandLine, test.input)), test.input)
 	}
+}
+
+func (s *StatsPusher) TestNewConfig() {
+	tests := []struct {
+		description string
+		flagSet     *pflag.FlagSet
+		viper       *viper.Viper
+	}{
+		{"defaults", nil, nil},
+		{"specified flagset", pflag.NewFlagSet(uuid.New(), pflag.ExitOnError), nil},
+		{"specified viper", nil, viper.New()},
+		{"specified both", pflag.NewFlagSet(uuid.New(), pflag.ExitOnError), viper.New()},
+	}
+
+	for _, test := range tests {
+		// Reset the default CommandLine flags between runs
+		pflag.CommandLine = pflag.NewFlagSet(uuid.New(), pflag.ExitOnError)
+		s.NotNil(newConfig(test.flagSet, test.viper), test.description)
+	}
+}
+
+func (s *StatsPusher) TestLoadConfig() {
+	tests := []struct {
+		description string
+		setFlags    bool
+		writeConfig bool
+		expectedErr bool
+	}{
+		{"nothing set", false, false, true},
+		{"flags only", true, false, false},
+		{"config file", false, true, false},
+		{"flags and config", true, true, false},
+	}
+
+	for _, test := range tests {
+		config, _, _, configFile, err := newTestConfig(test.setFlags, test.writeConfig, s.configData)
+		if configFile != nil {
+			defer func() { _ = os.Remove(configFile.Name()) }()
+		}
+		if !s.NoError(err, test.description) {
+			continue
+		}
+
+		err = config.loadConfig()
+		if test.expectedErr {
+			s.Error(err, test.description)
+		} else {
+			s.NoError(err, test.description)
+		}
+	}
+}
+
+func (s *StatsPusher) TestSetupLogging() {
+	s.NoError(s.config.setupLogging(), "failed to setup logging")
+	s.Equal(s.configData.LogLevel, logrus.GetLevel().String())
+}
+
+func (s *StatsPusher) TestValidate() {
+	tests := []struct {
+		description    string
+		coordinatorURL string
+		requestTimeout uint
+		datasetTTL     uint
+		bundleTTL      uint
+		nodeTTL        uint
+		expectedErr    string
+	}{
+		{"valid", "unix:///tmp/foobar", 5, 4, 3, 2, ""},
+		{"missing coord", "", 5, 4, 3, 2, "missing coordinator url"},
+		{"invalud coord", "asdf", 5, 4, 3, 2, "invalid coordinator url"},
+		{"invalid request timeout", "unix:///tmp/foobar", 0, 4, 3, 2, "request timeout must be > 0"},
+		{"invalid dataset ttl", "unix:///tmp/foobar", 5, 0, 3, 2, "dataset ttl must be > 0"},
+		{"invalid bundle ttl", "unix:///tmp/foobar", 5, 4, 0, 2, "bundle ttl must be > 0"},
+		{"invalid node ttl", "unix:///tmp/foobar", 5, 4, 3, 0, "node ttl must be > 0"},
+	}
+
+	for _, test := range tests {
+		configData := &ConfigData{
+			CoordinatorURL: test.coordinatorURL,
+			RequestTimeout: test.requestTimeout,
+			DatasetTTL:     test.datasetTTL,
+			BundleTTL:      test.bundleTTL,
+			NodeTTL:        test.nodeTTL,
+		}
+
+		config, fs, v, _, err := newTestConfig(true, false, configData)
+		if !s.NoError(err, test.description) {
+			continue
+		}
+		// Bind here to avoid the need for Load
+		s.Require().NoError(v.BindPFlags(fs), test.description)
+
+		err = config.validate()
+		if test.expectedErr != "" {
+			s.EqualError(err, test.expectedErr, test.description)
+		} else {
+			s.NoError(err, test.description)
+		}
+	}
+}
+
+func (s *StatsPusher) TestCoordinatorURL() {
+	u, err := url.ParseRequestURI(s.configData.CoordinatorURL)
+	s.Require().NoError(err)
+	s.Equal(u, s.config.coordinatorURL())
+}
+
+func (s *StatsPusher) TestRequestTimeout() {
+	s.EqualValues(s.configData.RequestTimeout, s.config.requestTimeout()/time.Second)
+}
+
+func (s *StatsPusher) TestDatasetTTL() {
+	s.EqualValues(s.configData.DatasetTTL, s.config.datasetTTL()/time.Second)
+}
+
+func (s *StatsPusher) TestBundleTTL() {
+	s.EqualValues(s.configData.BundleTTL, s.config.bundleTTL()/time.Second)
+}
+
+func (s *StatsPusher) TestNodeTTL() {
+	s.EqualValues(s.configData.NodeTTL, s.config.nodeTTL()/time.Second)
+}
+
+func newTestConfig(setFlags, writeConfig bool, configData *ConfigData) (*config, *pflag.FlagSet, *viper.Viper, *os.File, error) {
+	fs := pflag.NewFlagSet(uuid.New(), pflag.ExitOnError)
+	v := viper.New()
+	v.SetConfigType("json")
+	config := newConfig(fs, v)
+	if config == nil {
+		return nil, nil, nil, nil, errors.New("failed to return a config")
+	}
+
+	var configFile *os.File
+	if writeConfig {
+		var err error
+		configFile, err = ioutil.TempFile("", "statspusherTest-")
+		if err != nil {
+			return nil, nil, nil, nil, err
+		}
+		defer func() { _ = configFile.Close() }()
+
+		configJSON, _ := json.Marshal(configData)
+		if _, err := configFile.Write(configJSON); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+
+		if err := fs.Set("configFile", configFile.Name()); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+	}
+
+	if setFlags {
+		if err := fs.Set("coordinatorURL", configData.CoordinatorURL); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+		if err := fs.Set("logLevel", configData.LogLevel); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+		if err := fs.Set("requestTimeout", strconv.FormatUint(uint64(configData.RequestTimeout), 10)); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+		if err := fs.Set("datasetTTL", strconv.FormatUint(uint64(configData.DatasetTTL), 10)); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+		if err := fs.Set("bundleTTL", strconv.FormatUint(uint64(configData.BundleTTL), 10)); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+		if err := fs.Set("nodeTTL", strconv.FormatUint(uint64(configData.NodeTTL), 10)); err != nil {
+			return nil, nil, nil, configFile, err
+		}
+	}
+
+	return config, fs, v, configFile, nil
 }
