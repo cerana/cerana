e460d24a2d3ca66fcd78121d614d36f57607d58b
Brian Bickerton <brian.bickerton@gmail.com>
2016-05-06 20:51:41 +0000

    Statspusher command and main entrypoint

--- cmd/statspusher/main.go
+++ cmd/statspusher/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "github.com/spf13/pflag"
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/cerana/cerana/pkg/logrusx"
+	"github.com/spf13/pflag"
+)
 
 func main() {
-	_ = newConfig(nil, nil)
+	logrus.SetFormatter(&logrusx.MistifyFormatter{})
+
+	config := newConfig(nil, nil)
 	pflag.Parse()
+
+	dieOnError(config.loadConfig())
+	dieOnError(config.setupLogging())
+
+	sp, err := newStatsPusher(config)
+	dieOnError(err)
+
+	dieOnError(sp.run())
+	sp.stopOnSignal()
+}
+
+func dieOnError(err error) {
+	if err != nil {
+		logrus.Fatal("encountered an error during startup")
+	}
 }
