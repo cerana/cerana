da4c1c9ee5f22c8807d52a9422cba26aba9fb94b
Steve Isaacs <sisaacs@omniti.com>
2016-05-19 13:09:25 -0400

    This script is no longer needed and most of what it was doing is now part of the default job configuration.

--- build/nix-builder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This sets the path to point to the nix tools.
-source ~/.nix-profile/etc/profile.d/nix.sh
-
-export LC_ALL=C
-time nix-build $*
-if [ -h result/etc ]; then echo Error: Build resulted /etc as symlink && exit 1; fi
-nix-store -q result --graph | sed 's/#ff0000/#ffffff/' | dot -Nstyle=bold -Tpng > dependencies.png
-nix-store -q result --tree > dependencies.txt
