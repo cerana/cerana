a2abb0517ddfcf50f3dc3a2268328934b7d8250a
Manuel Mendez <mmendez534@gmail.com>
2016-06-01 15:18:52 -0400

    providers/kv: let Mock choose the kv addr
    There is no need for the Mock user to be concerned about the underlying kv store, so
    we will just let Mock handle the kv address.

--- providers/kv/mock.go
+++ providers/kv/mock.go
@@ -1,21 +1,19 @@
 package kv
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net"
-	"net/url"
 	"os"
 	"os/exec"
-	"path"
 
 	"github.com/cerana/cerana/acomm"
 	"github.com/cerana/cerana/internal/tests/common"
+	"github.com/cerana/cerana/provider"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 // Mock is a mock KV provider
 type Mock struct {
-	*KV
+	kv  *KV
 	dir string
 	cmd *exec.Cmd
 }
@@ -23,49 +21,30 @@ type Mock struct {
 // NewMock starts up a kv backend server and instantiates a new kv.KV provider.
 // The kv backend is started on the port provided as part of config.Address().
 // Mock.Stop() should be called when testing is done in order to clean up.
-func NewMock(config *Config, tracker *acomm.Tracker) (*Mock, error) {
-	dir, err := ioutil.TempDir("", "mock-kv-provider-")
+func NewMock(config *provider.Config, tracker *acomm.Tracker) (*Mock, error) {
+	s := common.Suite{}
+	s.KVPrefix = "mock-kv-provider"
+	s.KVCmdMaker = common.ConsulMaker
+	s.SetupSuite()
+
+	v := viper.New()
+	flagset := pflag.NewFlagSet("mock-kv-provider", pflag.PanicOnError)
+	conf := NewConfig(flagset, v)
+	s.Require().NoError(flagset.Parse(nil))
+	v.Set("service_name", "mock-kv-provider")
+	v.Set("socket_dir", config.SocketDir())
+	v.Set("coordinator_url", config.CoordinatorURL())
+	v.Set("log_level", "fatal")
+	v.Set("address", s.KVURL)
+	s.Require().NoError(config.LoadConfig())
+	s.Require().NoError(config.SetupLogging())
+
+	kv, err := New(conf, tracker)
 	if err != nil {
 		return nil, err
 	}
 
-	addr, err := config.Address()
-	if err != nil {
-		return nil, err
-	}
-
-	u, err := url.Parse(addr)
-	if err != nil {
-		return nil, err
-	}
-
-	_, pStr, err := net.SplitHostPort(u.Host)
-	if err != nil {
-		return nil, err
-	}
-
-	port := uint16(0)
-	_, err = fmt.Sscanf(pStr, "%d", &port)
-	if err != nil {
-		return nil, err
-	}
-
-	cmd := common.ConsulMaker(port, dir, path.Base(dir))
-	err = cmd.Start()
-	if err != nil {
-		return nil, err
-	}
-
-	kv, err := New(nil, tracker)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Mock{
-		KV:  kv,
-		cmd: cmd,
-		dir: dir,
-	}, nil
+	return &Mock{kv: kv, cmd: s.KVCmd, dir: s.KVDir}, nil
 }
 
 // Stop will stop the kv and remove the temporary directory used for it's data
