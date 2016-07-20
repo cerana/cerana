a4b7f743bcd4af6ae0ed8dd4e3107d9a357cd66d
Manuel Mendez <mmendez534@gmail.com>
2016-07-20 10:52:01 -0400

    providers/dhcp: store dhcp paramater in kv if new
    If called with any dhcp parameters, `dhcp-provider` will store the given
    configuration in the cluster configuration if no other configuration exists.

--- cmd/dhcp-provider/main.go
+++ cmd/dhcp-provider/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"net"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/cerana/cerana/acomm"
+	"github.com/cerana/cerana/pkg/logrusx"
 	"github.com/cerana/cerana/provider"
+	"github.com/cerana/cerana/providers/clusterconf"
 	"github.com/cerana/cerana/providers/dhcp"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -19,8 +24,67 @@ func dieOnError(msg string, err error) {
 }
 
 func defaultNetwork() net.IPNet {
-	_, net, _ := net.ParseCIDR("10.0.0.0/8")
+	_, net, _ := net.ParseCIDR("172.16.0.0/16")
 	return *net
+}
+
+func comm(tracker *acomm.Tracker, coordinator *url.URL, task string, args interface{}, resp interface{}) error {
+	ch := make(chan *acomm.Response, 1)
+	handler := func(_ *acomm.Request, resp *acomm.Response) {
+		ch <- resp
+	}
+
+	req, err := acomm.NewRequest(acomm.RequestOptions{
+		Task:           task,
+		Args:           args,
+		ResponseHook:   tracker.URL(),
+		SuccessHandler: handler,
+		ErrorHandler:   handler,
+	})
+	dieOnError("create request object", err)
+	dieOnError("track request", tracker.TrackRequest(req, 0))
+	dieOnError("send request", acomm.Send(coordinator, req))
+
+	aResp := <-ch
+	if aResp.Error != nil {
+		return aResp.Error
+	}
+
+	dieOnError("deserialize result", aResp.UnmarshalResult(resp))
+	return nil
+}
+
+func getDHCPConfig(tracker *acomm.Tracker, coordinator *url.URL) *clusterconf.DHCPConfig {
+	dconf := &clusterconf.DHCPConfig{}
+	err := comm(tracker, coordinator, "get-dhcp", nil, dconf)
+	dieOnError("get dhcp configuration", err)
+	return dconf
+}
+
+func setDHCPConfig(tracker *acomm.Tracker, coordinator *url.URL, config *dhcp.Config) {
+	dconf := clusterconf.DHCPConfig{}
+	err := comm(tracker, coordinator, "get-dhcp", nil, &dconf)
+	if err == nil {
+		logrus.Warn("dhcp configuration exists, not overriding it")
+		return
+	}
+
+	dconf = clusterconf.DHCPConfig{
+		DNS:      config.DNSServers(),
+		Duration: config.LeaseDuration(),
+		Gateway:  config.Gateway(),
+		Net:      *config.Network(),
+	}
+	err = comm(tracker, coordinator, "set-dhcp", dconf, nil)
+	dieOnError("set dhcp configuration", err)
+}
+
+func joinDNS(dns []net.IP) string {
+	strs := make([]string, len(dns))
+	for i := range dns {
+		strs[i] = dns[i].String()
+	}
+	return strings.Join(strs, ",")
 }
 
 func main() {
@@ -38,12 +102,26 @@ func main() {
 	dieOnError("load configuration", config.LoadConfig())
 	dieOnError("setup logging", config.SetupLogging())
 
+	set := v.IsSet("dns-servers") || v.IsSet("gateway") || v.IsSet("lease-duration") || v.IsSet("network")
 
 	server, err := provider.NewServer(config.Config)
 	dieOnError("create provider", err)
+	dieOnError("start tracker", server.Tracker().Start())
+
+	if set == true {
+		setDHCPConfig(server.Tracker(), config.CoordinatorURL(), config)
+	}
+	storedConfig := getDHCPConfig(server.Tracker(), config.CoordinatorURL())
+
+	v.Set("dns-servers", joinDNS(storedConfig.DNS))
+	v.Set("gateway", storedConfig.Gateway)
+	v.Set("lease-duration", storedConfig.Duration)
+	v.Set("network", storedConfig.Net.String())
 
 	d, err := dhcp.New(config, server.Tracker())
 	dieOnError("create dhcp server", err)
+
+	logrus.WithField("config", storedConfig).Info("starting provider")
 
 	d.RegisterTasks(server)
 	if len(server.RegisteredTasks()) == 0 {
