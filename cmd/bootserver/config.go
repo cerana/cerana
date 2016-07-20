2823aa4c468d341aef567b49089777146857b8ca
Manuel Mendez <mmendez534@gmail.com>
2016-07-20 17:56:18 -0400

    cmd: add bootserver

---
+++ cmd/bootserver/config.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/cerana/cerana/provider"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	*provider.Config
+	viper *viper.Viper
+}
+
+func newConfig(f *pflag.FlagSet, v *viper.Viper) config {
+	f.String("dhcp-interface", "", "interface to bind dhcp server to")
+	f.String("ipxe", "undionly.pxe", "iPXE file to serve to non-iPXE clients")
+	f.String("kernel", "kernel", "kernel file")
+	f.String("initrd", "initrd", "initrd file")
+	return config{
+		Config: provider.NewConfig(f, v),
+		viper:  v,
+	}
+}
+
+func (c config) LoadConfig() error {
+	if err := c.Config.LoadConfig(); err != nil {
+		return err
+	}
+
+	return c.validate()
+}
+
+func (c config) validate() error {
+	if c.viper.GetString("dhcp-interface") == "" {
+		return errors.New("interface is not defined")
+	}
+	return c.Config.Validate()
+}
+
+func (c config) iface() string {
+	return c.viper.GetString("dhcp-interface")
+}
+
+func (c config) iPXE() string {
+	return c.viper.GetString("ipxe")
+}
+
+func (c config) kernel() string {
+	return c.viper.GetString("kernel")
+}
+
+func (c config) initrd() string {
+	return c.viper.GetString("initrd")
+}
