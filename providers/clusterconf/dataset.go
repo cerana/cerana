4d9a7dcf86fbeb30bd81bc1b2f1218419725f29f
Brian Bickerton <brian.bickerton@gmail.com>
2016-08-01 21:30:40 +0000

    clusterconf - Cleaner parsing of ids from kv keys

--- providers/clusterconf/dataset.go
+++ providers/clusterconf/dataset.go
@@ -3,9 +3,10 @@ package clusterconf
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/url"
 	"path"
-	"strings"
+	"path/filepath"
 	"sync"
 
 	"github.com/cerana/cerana/acomm"
@@ -64,10 +65,13 @@ func (c *ClusterConf) ListDatasets(req *acomm.Request) (interface{}, *url.URL, e
 	}
 	// extract and deduplicate the dataset ids
 	ids := make(map[string]bool)
+	keyFormat := filepath.Join(datasetsPrefix, "%s")
 	for _, key := range keys {
-		// keys are full paths and include all child keys.
-		// e.g. {prefix}/{id}/{rest/of/path}
-		id := strings.Split(strings.TrimPrefix(key, datasetsPrefix), "/")[0]
+		var id string
+		_, err := fmt.Sscanf(key, keyFormat, &id)
+		if err != nil {
+			return nil, nil, errors.New("invalid dataset id")
+		}
 		ids[id] = true
 	}
 
