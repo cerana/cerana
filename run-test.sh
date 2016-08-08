bb655d4099f951ab483670e984eb1f84e1d7840c
Manuel Mendez <mmendez534@gmail.com>
2016-08-08 17:52:47 -0400

    run-test.sh: fixes to be shellcheck clean

--- run-test.sh
+++ run-test.sh
@@ -5,20 +5,20 @@ set -e
 # args:
 # $1: relative path to .test file
 
-dir=$(dirname $1)
-name=$(basename $1)
+dir=$(dirname "$1")
+name=$(basename "$1")
 out="$dir/test.out"
-exec 2> >(tee $out)
+exec 2> >(tee "$out")
 
 which consul &>/dev/null
 
 cid=$(docker run -dti \
 	--cap-add SYS_ADMIN \
 	--device  /dev/zfs:/dev/zfs \
-	--env     TMPDIR=$(mktemp -d) \
-	--env     KV=${KV:-consul} \
-	--name    $name \
-	--volume  $PWD:/mistify:ro \
+	--env     TMPDIR="$(mktemp -d)" \
+	--env     KV="${KV:-consul}" \
+	--name    "$name" \
+	--volume  "$PWD:/mistify:ro" \
 	--volume  /sys/fs/cgroup:/sys/fs/cgroup:ro \
 	--volume  /tmp:/tmp \
 	mistifyio/mistify-os:zfs-stable-api
@@ -27,13 +27,13 @@ cid=$(docker run -dti \
 [[ -n $cid ]]
 sleep .25
 
-docker cp $(which consul) $cid:/usr/bin/
-docker cp $(which etcd) $cid:/usr/bin/
+docker cp "$(which consul)" "$cid:/usr/bin/"
+docker cp "$(which etcd)" "$cid:/usr/bin/"
 
-docker exec $cid sh -c "cd /mistify/$dir; ./$name -test.v" >&2 || ret=$?;
+docker exec "$cid" sh -c "cd '/mistify/$dir'; './$name' -test.v" >&2 || ret=$?
 
-docker kill  $cid > /dev/null || :
-docker rm -v $cid > /dev/null || :
+docker kill  "$cid" > /dev/null || :
+docker rm -v "$cid" > /dev/null || :
 echo "### TEST  $name"
-cat $out
+cat "$out"
 exit $ret
