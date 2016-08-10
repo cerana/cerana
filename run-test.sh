41b03b2f1c750369514d0993592f2fd65a9cc300
Manuel Mendez <mmendez534@gmail.com>
2016-08-10 16:26:06 -0400

    run-test.sh: simplify the test runner script
    No need to cd to the test binary's directory since the go test takes care of
    that.

--- run-test.sh
+++ run-test.sh
@@ -27,8 +27,8 @@ sleep .25
 
 docker cp "$(which consul)" "$cid:/usr/bin/"
 docker cp "$(which etcd)" "$cid:/usr/bin/"
-
-docker exec "$cid" sh -c "cd /mistify/$dir; echo '### TEST  $name'; ./$name -test.v" >&2 || ret=$?
+docker exec -i "$cid" sh -c "echo '### TEST  $name'; '/mistify/$1' -test.v" >&2
+ret=$?
 
 docker kill "$cid" >/dev/null || :
 docker rm -v "$cid" >/dev/null || :
