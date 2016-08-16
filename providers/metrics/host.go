92719b3165f693f4ba6a0e89c99b080a0fb731b5
Brian Bickerton <brian.bickerton@gmail.com>
2016-08-16 21:16:39 +0000

    providers/metrics - Wrap errors

--- providers/metrics/host.go
+++ providers/metrics/host.go
@@ -4,11 +4,12 @@ import (
 	"net/url"
 
 	"github.com/cerana/cerana/acomm"
+	"github.com/cerana/cerana/pkg/errors"
 	"github.com/shirou/gopsutil/host"
 )
 
 // Host returns information about the host machine.
 func (m *Metrics) Host(req *acomm.Request) (interface{}, *url.URL, error) {
 	hostInfo, err := host.Info()
-	return hostInfo, nil, err
+	return hostInfo, nil, errors.Wrap(err)
 }
