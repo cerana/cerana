309507c434cf4e53447cdd47f50544dec59d57ba
Nahum Shalman <nshalman@omniti.com>
2016-08-25 20:34:00 +0000

    Reformat all scripts under build with shfmt

--- build/plugins.sh
+++ build/plugins.sh
@@ -14,15 +14,15 @@ REF=/usr/share/jenkins/ref/plugins
 mkdir -p $REF
 
 while read spec || [ -n "$spec" ]; do
-    plugin=(${spec//:/ });
+    plugin=(${spec//:/ })
     [[ ${plugin[0]} =~ ^# ]] && continue
     [[ ${plugin[0]} =~ ^\s*$ ]] && continue
     [[ -z ${plugin[1]} ]] && plugin[1]="latest"
     echo "Downloading ${plugin[0]}:${plugin[1]}"
 
     if [ -z "$JENKINS_UC_DOWNLOAD" ]; then
-      JENKINS_UC_DOWNLOAD=$JENKINS_UC/download
+        JENKINS_UC_DOWNLOAD=$JENKINS_UC/download
     fi
     curl -sSL -f ${JENKINS_UC_DOWNLOAD}/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.jpi
     unzip -qqt $REF/${plugin[0]}.jpi
-done  < $1
+done <$1
