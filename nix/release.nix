9947ae5318d65f5d581f0167dcbb49f7698e74a2
Nahum Shalman <nshalman@omniti.com>
2018-02-13 02:01:55 +0000

    move iso building into nix expression

--- nix/release.nix
+++ nix/release.nix
@@ -7,7 +7,6 @@ let
     let
       config_evaled = import "${pkgs.path}/nixos/lib/eval-config.nix" config;
       build = config_evaled.config.system.build;
-      kernelTarget = config_evaled.pkgs.stdenv.platform.kernelTarget;
     in
       pkgs.symlinkJoin {
         name="netboot";
@@ -18,7 +17,6 @@ let
           build.ceranaGrub2Config
         ];
       };
-
 in rec {
 
   ipxe = pkgs.ipxe;
@@ -33,6 +31,26 @@ in rec {
       ./modules/profiles/cerana-hardware.nix
       ./modules/profiles/ceranaos.nix
     ];
+  };
+
+  minimal_iso = pkgs.stdenv.mkDerivation {
+  system = "x86_64-linux";
+  name = "cerana-iso";
+  src = minimal_media;
+  installPhase =
+    ''
+      ${pkgs.grub2}/bin/grub-mkrescue \
+        -o $out \
+        -V CERANA \
+        --xorriso=${pkgs.xorriso}/bin/xorriso \
+        -- \
+        -follow on \
+        -pathspecs on \
+        -add boot/grub/grub.cfg=${minimal_media}/grub.cfg \
+        bzImage=${minimal_media}/bzImage \
+        initrd=${minimal_media}/initrd \
+        ipxe.lkrn=${pkgs.ipxe}/ipxe.lkrn
+    '';
   };
 
 }
